54b31795aea0e87c459a8088ea24a3703dc942c8
Raj Croager <rcroager@marjentech.com.>
2025-02-25 10:02:59 -0600

    Minor edits to main.tf

--- tf_main.tf
+++ tf_main.tf
@@ -4,12 +4,8 @@
       source  = "kreuzwerker/docker"
       version = ">= 3.0.2" # Adjust the version constraint as needed
     }
-    enclave = {
-      enrolement_key = var.enclave_enrolment_key
-    }
   }
 }
-
 
 resource "docker_container" "enclave" {
   name         = "enclave"
@@ -21,6 +17,9 @@ resource "docker_container" "enclave" {
   devices {
     host_path = "/dev/net/tun"
   }
+  env = [
+    "ENCLAVE_ENROLMENT_KEY=${var.enclave_enrolment_key}"
+  ]
   volumes {
     volume_name    = docker_volume.enclave_config.name
     container_path = "/etc/enclave/profiles"
@@ -32,7 +31,6 @@ resource "docker_container" "enclave" {
   restart = "unless-stopped"
 }
 
-
 resource "docker_container" "watchtower" {
   image = "containrrr/watchtower"
   name  = "watchtower"
@@ -43,11 +41,9 @@ resource "docker_container" "watchtower" {
   restart = "unless-stopped"
 }
 
-
 resource "docker_volume" "enclave_config" {
   name = "enclave-config"
 }
-
 
 resource "docker_volume" "enclave_logs" {
   name = "enclave-logs"
